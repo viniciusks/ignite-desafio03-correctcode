78b1202e48779e4d02515e962a92dedf1fce7f12
vinicius kremer <vinicius.kremer@gmail.com>
2021-03-15 08:55:46 -0300

    Finalizando a correção

---
+++ setupTest.js
@@ -0,0 +1,1 @@
+jest.setTimout(400);
--- src/index.js
+++ src/index.js
@@ -23,6 +23,8 @@ app.post("/repositories", (request, response) => {
         likes: 0,
     };
 
+    repositories.push(repository);
+
     return response.status(201).json(repository);
 });
 
@@ -30,7 +32,7 @@ app.put("/repositories/:id", (request, response) => {
     const { id } = request.params;
     const { title, url, techs } = request.body;
 
-    repositoryIndex = repositories.findindex((repository) => repository.id === id);
+    repositoryIndex = repositories.findIndex((repository) => repository.id === id);
 
     if (repositoryIndex < 0) {
         return response.status(404).json({ error: "Repository not found" });
@@ -52,13 +54,15 @@ app.delete("/repositories/:id", (request, response) => {
 
     repositoryIndex = repositories.findIndex((repository) => repository.id === id);
 
-    if (repositoryIndex > 0) {
+    if (repositoryIndex < 0) {
         return response.status(404).json({ error: "Repository not found" });
     }
 
     repositories.splice(repositoryIndex, 1);
 
-    return response.status(204).send();
+    return response.status(204).json({
+        message: "Delete repository",
+    });
 });
 
 app.post("/repositories/:id/like", (request, response) => {
@@ -70,9 +74,9 @@ app.post("/repositories/:id/like", (request, response) => {
         return response.status(404).json({ error: "Repository not found" });
     }
 
-    const likes = repositories[repositoryIndex].likes + 1;
+    const likesObj = { likes: ++repositories[repositoryIndex].likes };
 
-    return response.json(likes);
+    return response.json(likesObj);
 });
 
 module.exports = app;
